c78b46c52e4f4726bb27d5cf1bd1e54f488278ae
Jai Govindani <govindani@gmail.com>
2017-07-31 07:40:49 +0700

    Start work on flatten() function

--- index.go
+++ index.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"sort"
 	"encoding/csv"
 	"fmt"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -23,5 +26,81 @@ func main() {
 		panic(err)
 	}
 
+	localmongo.SetMode(mgo.Monotonic, true)
+
+	//Now we have the collection names in collections
+	for _, collectionName := range collections {
+
+		if collectionName == "RBShop" {
+			var line bson.M
+			currentCollection := ramenbeastdb.C(collectionName)
+			err = currentCollection.Find(nil).One(&line)
+			if err != nil {
+				log.Fatal(err)
+			}
+			
+			var headers []string
+			for column := range line {
+				headers = append(headers, fmt.Sprint(column))
+			}
+
+			//Sort headers otherwise they'll be random?
+			sort.Strings(headers)
+
+			//Create a file
+			file, err := os.Create(collectionName+".csv")
+			if err != nil {
+				fmt.Println("ERROR: Cannot create file")
+			}
+			defer file.Close()
+
+			writer := csv.NewWriter(file)
+			defer writer.Flush()
+
+			writer.Write(headers)
+			fmt.Printf("Printing headers for collection %v\n", collectionName)
+			fmt.Println(headers)
+
+			iteratingCursor := currentCollection.Find(nil).Iter()
+			for iteratingCursor.Next(&line) {
+				fmt.Println(line)
+
+				valueArray := make([]string, len(line))
+				for _, value := range line {
+					valueArray = append(valueArray, value.(string))
+				}
+
+				writer.Write(valueArray)
+			}
+
+			if err := iteratingCursor.Close(); err != nil {
+				panic(err)
+			}
+		}
+	}
 	
+}
+
+func flatten(input bson.M, lkey string, flattened *map[string]interface{}) {
+	for rkey, value := range input {
+		key := lkey + rkey
+
+		if _, ok := value.(string); ok {
+			(*flattened)[key] = value.(string)
+		} else if _, ok := value.(float64); ok {
+			(*flattened)[key] = value.(float64)
+		} else if _, ok = value.(int); ok {
+			(*flattened)[key] = value.(int)
+		} else if _, ok = value.(int64); ok {
+			(*flattened)[key] = value.(int64)
+		} else if _, ok = value.(bool); ok {
+			(*flattened)[key] = value.(bool)
+		} else if _, ok = value.(time.Time); ok {
+			(*flattened)[key] = value.(time.Time).Format("2006-01-02T15:04:05Z07:00")
+		} else if _, ok := value.(bson.ObjectId); ok {
+			(*flattened)[key] = value.(bson.ObjectId).Hex
+		} else if _, ok := value.([]interface{}); ok {
+			
+		}
+	}
 }
